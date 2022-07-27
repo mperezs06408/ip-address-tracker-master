19e587f336a008dda805e71884e00e9e48a9ad56
mperezs06408 <mperezs06408@universidadean.edu.co>
2022-07-27 06:50:40 -0500

    TEMPORAL COMMIT TO UPDATE CHANGES TO REMOTE REPOSITORY

--- src/components/Vmap.js
+++ src/components/Vmap.js
@@ -1,8 +1,60 @@
 app.component('v-map', {
-    template: 
-    `
+    data(){
+        return {
+            API_KEY: 'at_EeOtS6FiVqmtF9Z919UVqgeVrgKaW',
+            latitude: 0,
+            longitude: 0,
+            currentIP: '0.0.0.0'
+        }
+    },
+    template: `
     <section class="map-section" id="map">
-        <h2>Hello Map</h2>
     </section>
-    `
+    `,
+    methods: {
+        getIPLocation: async function(IP_ADDRESS) {
+            const ipifyAPI = 
+                await fetch(
+                    `https://geo.ipify.org/api/v2/country,city?apiKey=${this.API_KEY}&ipAddress=${IP_ADDRESS}`
+                );
+            
+            const result = await ipifyAPI.json();
+
+            this.latitude = result.location.lat;
+            this.longitude = result.location.lng;
+
+        },
+        initMap: async function() {
+            const map = L.map('map', {
+                zoomControl: false
+            });
+            
+            await this.getInitialIP();
+            await this.getIPLocation(this.currentIP);
+
+            const location = [this.latitude, this.longitude];
+
+            map.setView(location, 13);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '© OpenStreetMap'
+            }).addTo(map);
+
+
+
+            const markerLayer = L.marker(location);
+
+            markerLayer.addTo('map');
+        },
+        getInitialIP: async function() {
+            const API = await fetch('https://api.ipify.org?format=json');
+
+            const result = await API.json();
+
+            this.currentIP = result.ip;
+        },
+    },
+    mounted() {
+        this.initMap();
+    },
 });
